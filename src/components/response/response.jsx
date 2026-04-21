be743e91660261cd13079c8429aecabb415c53a0
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2026-04-21 23:21:27 +0100

    fix: current review and loading logic

--- src/components/response/response.jsx
+++ src/components/response/response.jsx
@@ -5,21 +5,23 @@ import Button from "../button/button";
 import "./response.css";
 
 export default function Response({ ref }) {
-  const { currentReview, error, loading, reset } = useReview();
+  const { currentReview, reset } = useReview();
 
   return (
     <div ref={ref} className="trycatch-response">
-      {loading && <Loading />}
-      {currentReview && (
+      {currentReview?.loading && <Loading />}
+
+      {currentReview?.result && (
         <>
           <h2>Your code review</h2>
+
           <div className="trycatch-response-container">
-            <ReactMarkdown>{currentReview}</ReactMarkdown>
+            <ReactMarkdown>{currentReview.result}</ReactMarkdown>
           </div>
+
           <Button onClick={reset}>Review another snippet</Button>
         </>
       )}
-      {error && <h2>Sorry, an error occurred. Please try again later.</h2>}
     </div>
   );
 }
